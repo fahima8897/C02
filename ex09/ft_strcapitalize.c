8ad41b01e80972e356635dfa45b67a2aa7c36d4c
Fahima BOUMELLIL <fboumell@e3r7p2.42.fr>
2021-03-19 15:33:47 +0100

    a

--- ex09/ft_strcapitalize.c
+++ ex09/ft_strcapitalize.c
@@ -6,16 +6,16 @@
 /*   By: fboumell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 15:35:27 by fboumell          #+#    #+#             */
-/*   Updated: 2021/03/17 15:54:33 by fboumell         ###   ########.fr       */
+/*   Updated: 2021/03/19 15:31:54 by fboumell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strcapitalize(char *str)
 {
-	int i;
+	unsigned int i;
 
-	if (*str != '\0' && *str >= 'a' && *str <= 'z')
-			*str = *str - 32;
+	if (str[0] != '\0' && str[0] >= 'a' && str[0] <= 'z')
+			str[0] = str[0] - 32;
 	i = 1;
 	while (str[i] != '\0')
 	{
@@ -24,7 +24,7 @@ char	*ft_strcapitalize(char *str)
 					(str[i - 1] < '0' || 
 					(str[i - 1] > '9' && str[i - 1] < 'A') ||
 					(str[i - 1] > 'Z' && str[i - 1] < 'a') ||
-					str[i - 1]))
+					str[i - 1] > 'z'))
 		{
 			if (str[i] >= 'a' && str[i] <= 'z')
 				str[i] = str[i] - 32;
@@ -40,6 +40,6 @@ char	*ft_strcapitalize(char *str)
 
 int main()
 {
-	char str[] = "coucou 42++pourauoi hdoiewhfoi ?";
-	printf("%", ft_strcapitalizer(str));
+	char str[] = "sqlut, comment tu vas? 42mots quarante-deux; cinquante+et+un";
+	printf("%s", ft_strcapitalize(str));
 }
