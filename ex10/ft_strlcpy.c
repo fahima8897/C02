d12c8d3afec518d4b50fd7e91eaafedb1dc9a441
Fahima BOUMELLIL <fboumell@e3r7p2.42.fr>
2021-03-19 15:46:42 +0100

    a

---
+++ ex10/ft_strlcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fboumell <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/19 15:37:53 by fboumell          #+#    #+#             */
+/*   Updated: 2021/03/19 15:44:54 by fboumell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int i;
+	unsigned int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+	j = 0;
+	while (src[j] != '\0' && j < size)
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	i = 0;
+	while (src[i])
+		i++;
+	return (i);
+}
